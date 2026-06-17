77f8b2c183bb899e554ddba968d38b721f3dfe29
amoyba <dev@osti.com.ph>
2026-06-17 16:22:05 +0800

    cas3scw3caw

--- backend/routes/pages.js
+++ backend/routes/pages.js
@@ -17,9 +17,6 @@ function requireAuth(req, res, next) {
   }
 }
 
-  pageRouter.get("/onlinekiosk", (req, res) => {
-    res.sendFile(path.join(rootpath, "public/html","onlinekiosk.html"));
-  });
   pageRouter.get("/kiosk", (req, res) => {
     res.sendFile(path.join(rootpath, "public/html","kiosk.html"));
   });
