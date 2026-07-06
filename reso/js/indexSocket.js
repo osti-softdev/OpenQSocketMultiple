acc87a35b70c4bbfa4c6d521aecec176a2fbe907
Software Development Team <dev@osti.com.ph>
2026-07-06 12:03:38 +0800

    Update indexSocket.js

--- reso/js/indexSocket.js
+++ reso/js/indexSocket.js
@@ -448,7 +448,7 @@ function playNextAd() {
 		videoElement = document.getElementById("dep");
 
 	// Stop previous playback safely
-	videoElement.src = `http://127.0.0.1/ads/${encodedFile}`;
+	videoElement.src = `/ads/${encodedFile}`;
 	videoElement.autoplay = true;
 	videoElement.playsInline = true;
 	videoElement.load();
