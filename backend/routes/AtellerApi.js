c96bd04a67eaaeac0d7638e4aad9e20594f4432a
Software Development Team <dev@osti.com.ph>
2026-08-13 17:28:14 +0800

    asdasdasd

--- backend/routes/AtellerApi.js
+++ backend/routes/AtellerApi.js
@@ -454,7 +454,6 @@ module.exports = function createTellerApiRouter(io) {
     function callTicket(ticketId, tellerId, counterNumber, counter_group, counter_user, res) {
         const { date, time } = getPHDateTime();
         const finishEntry = `[${time}-${counter_user}-${counterNumber}-AutoFinished]`;
-        console.log(tellerId)
         // Auto-complete previous ticket this teller was serving
         db.run(
             `
--- index.js
+++ index.js
@@ -56,28 +56,32 @@ function createWindows() {
     });
 
     // 2. Create Display Window (on Extended Display, if available)
-    if (externalDisplay) {
-        displayWindow = new BrowserWindow({
-            x: externalDisplay.bounds.x,
-            y: externalDisplay.bounds.y,
-            width: externalDisplay.bounds.width,
-            height: externalDisplay.bounds.height,
-            fullscreen: true,
-            autoHideMenuBar: true,
-            webPreferences: {
-                nodeIntegration: false,
-                contextIsolation: true
-            }
-        });
+    // if (externalDisplay) {
+    displayWindow = new BrowserWindow({
+        // x: externalDisplay.bounds.x,
+        // y: externalDisplay.bounds.y,
+        // width: externalDisplay.bounds.width,
+        // height: externalDisplay.bounds.height,
+        x: primaryDisplay.bounds.x,
+        y: primaryDisplay.bounds.y,
+        width: primaryDisplay.bounds.width,
+        height: primaryDisplay.bounds.height,
+        fullscreen: true,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true
+        }
+    });
+    displayWindow.webContents.setZoomFactor(0.6);
+    displayWindow.loadURL(`${BASE_URL}/display`);
 
-        displayWindow.loadURL(`${BASE_URL}/display`);
-
-        displayWindow.on('closed', () => {
-            displayWindow = null;
-        });
-    } else {
-        console.log("No external display found for the /display window.");
-    }
+    displayWindow.on('closed', () => {
+        displayWindow = null;
+    });
+    // } else {
+    //     console.log("No external display found for the /display window.");
+    // }
 }
 
 // Quit when all windows are closed
